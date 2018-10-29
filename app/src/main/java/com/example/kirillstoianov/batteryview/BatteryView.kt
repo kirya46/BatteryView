796a1bf223015e3f6e29445a75b539e6d467f90b
kirill.stoianov <kirillstoianov46@gmail.com>
2018-10-29 18:53:36 +0200

    Added: battery background and body. OnWork: refactor and add sections.

--- app/src/main/java/com/example/kirillstoianov/batteryview/BatteryView.kt
+++ app/src/main/java/com/example/kirillstoianov/batteryview/BatteryView.kt
@@ -1,10 +1,11 @@
 package com.example.kirillstoianov.batteryview
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.view.View
-import android.os.Build
-import android.annotation.TargetApi
 import com.example.kirillstoianov.batteryview.util.Util
 
 
@@ -14,28 +15,75 @@ import com.example.kirillstoianov.batteryview.util.Util
 //class BatteryView(context: Context, attributeSet: AttributeSet?, deffStyle: Int) : View(context, attributeSet, deffStyle) {
 class BatteryView(context: Context) : View(context) {
 
-    val backgroundPaint: Paint
     var backgroundPath: Path
+    val backgroundPaint: Paint
+
+    var bodyPath: Path
+    var bodyPaint: Paint
+
+    var sectionPath: Path
+    var sectionPaintGreen: Paint
+    var sectionPaintTransparent: Paint
 
     init {
-        backgroundPaint = Paint()
-        backgroundPaint.color = Color.RED
-
         backgroundPath = Path()
+        backgroundPaint = Paint()
+//        backgroundPaint.color = Color.parseColor("#eaeaea")
+        backgroundPaint.color = Color.CYAN
+
+        bodyPath = Path()
+        bodyPaint = Paint()
+        bodyPaint.color = Color.parseColor("#8d8d8d")
+
+        sectionPath = Path()
+        sectionPaintGreen = Paint()
+        sectionPaintGreen.color = Color.GREEN
+        sectionPaintTransparent = Paint()
+        sectionPaintTransparent.color = Color.WHITE
+        sectionPaintTransparent.alpha = 20
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        initBackgroundPath()
+//        initPaths()
+        drawBattery(width,height)
+
 
         canvas?.drawPath(backgroundPath, backgroundPaint)
+        canvas?.drawPath(bodyPath, bodyPaint)
+        canvas?.drawPath(sectionPath, sectionPaintGreen)
+
     }
 
-    private fun initBackgroundPath() {
-        backgroundPath = Util().getBatteryBackground(
-                50f, 50f, 150f, 450f,
-                14f, 14f, false)
+    private fun drawBattery(width: Int, height: Int) {
+        val util = Util()
+        val outerCornerRadius = width/12f
+        backgroundPath = util.getBatteryBackground(
+                0f, 0f, width.toFloat(), height.toFloat(),
+                outerCornerRadius, outerCornerRadius,  outerCornerRadius - outerCornerRadius / 3, false)
+
+
+        val innerCornerRadius = width/24f
+        bodyPath = util.getBodyPath(0f, 0f, width.toFloat(), height.toFloat(),
+                innerCornerRadius, innerCornerRadius, outerCornerRadius)
     }
 
+    private fun initPaths() {
+        val util = Util()
+        val outerCornerRadius = 14f
+        backgroundPath = util.getBatteryBackground(
+                50f, 50f, 150f, 300f,
+                outerCornerRadius, outerCornerRadius, 50f + outerCornerRadius - outerCornerRadius / 3, false)
+
+
+        val innerCornerRadius = 6f
+        bodyPath = util.getBodyPath(50f, 50f, 150f, 300f,
+                innerCornerRadius, innerCornerRadius, outerCornerRadius)
+
+//        sectionPath = util.getSectionPath(
+//                50f, 50f, 150f, 300f,
+//                3f, 3f, outerCornerRadius
+//        )
+    }
 }
--- app/src/main/java/com/example/kirillstoianov/batteryview/util/Util.kt
+++ app/src/main/java/com/example/kirillstoianov/batteryview/util/Util.kt
@@ -97,40 +97,177 @@ class Util {
     }
 
 
-    fun getBatteryBackground(left: Float, top: Float, right: Float, bottom: Float, rx: Float, ry: Float, conformToOriginalPost: Boolean): Path {
+//    fun getBatteryBackground(left: Float, top: Float, right: Float, bottom: Float, rx: Float, ry: Float, conformToOriginalPost: Boolean): Path {
+//        var rx = rx
+//        var ry = ry
+//        val path = Path()
+//        if (rx < 0) rx = 0f
+//        if (ry < 0) ry = 0f
+//        val width = right - left
+////        val height = bottom - top  fixme: uncomment
+//        val height = bottom - (top - 200)
+//        if (rx > width / 2) rx = width / 2
+//        if (ry > height / 2) ry = height / 2
+//        val widthMinusCorners = width - 2 * rx
+//        val heightMinusCorners = height - 2 * ry
+//
+//        path.moveTo(right, top + ry)
+//        path.arcTo(RectF(right - 2 * rx, top, right, top + 2 * ry), 0f, -90f, false) //top-right-corner
+//        path.rLineTo(-widthMinusCorners, 0f)
+//        path.arcTo(RectF(left, top, left + 2 * rx, top + 2 * ry), 270f, -90f, false)//top-left corner.
+//        path.rLineTo(0f, heightMinusCorners)
+//        if (conformToOriginalPost) {
+//            path.rLineTo(0f, ry)
+//            path.rLineTo(width, 0f)
+//            path.rLineTo(0f, -ry)
+//        } else {
+//            path.arcTo(RectF(left, bottom - 2 * ry, left + 2 * rx, bottom), 180f, -90f, false) //bottom-left corner
+//            path.rLineTo(widthMinusCorners, 0f)
+//            path.arcTo(RectF(right - 2 * rx, bottom - 2 * ry, right, bottom), 90f, -90f, false) //bottom-right corner
+//        }
+//
+//        path.rLineTo(0f, -heightMinusCorners)
+//
+//        path.close()
+//        return path
+//    }
+
+    fun getBatteryBackground(left: Float, top: Float, right: Float, bottom: Float, rx: Float, ry: Float, tipPadding: Float, conformToOriginalPost: Boolean): Path {
         var rx = rx
         var ry = ry
         val path = Path()
         if (rx < 0) rx = 0f
         if (ry < 0) ry = 0f
+
+        val bodyTop = tipPadding
         val width = right - left
-//        val height = bottom - top  fixme: uncomment
-        val height = bottom - (top - 200)
+        val height = bottom - bodyTop
         if (rx > width / 2) rx = width / 2
         if (ry > height / 2) ry = height / 2
         val widthMinusCorners = width - 2 * rx
         val heightMinusCorners = height - 2 * ry
 
-        path.moveTo(right, top + ry)
-        path.arcTo(RectF(right - 2 * rx, top, right, top + 2 * ry), 0f, -90f, false) //top-right-corner
-        path.rLineTo(-widthMinusCorners, 0f)
-        path.arcTo(RectF(left, top, left + 2 * rx, top + 2 * ry), 270f, -90f, false)//top-left corner.
-        path.rLineTo(0f, heightMinusCorners)
-        if (conformToOriginalPost) {
-            path.rLineTo(0f, ry)
-            path.rLineTo(width, 0f)
-            path.rLineTo(0f, -ry)
-        } else {
-            path.arcTo(RectF(left, bottom - 2 * ry, left + 2 * rx, bottom), 180f, -90f, false) //bottom-left corner
-            path.rLineTo(widthMinusCorners, 0f)
-            path.arcTo(RectF(right - 2 * rx, bottom - 2 * ry, right, bottom), 90f, -90f, false) //bottom-right corner
-        }
+        path.moveTo(right, bodyTop + ry)
 
+        path.arcTo(RectF(right - 2 * rx, bodyTop, right, bodyTop + 2 * ry), 0f, -90f, false) //top right corner
+        path.rLineTo(-widthMinusCorners, 0f)
+
+        path.arcTo(RectF(left, bodyTop, left + 2 * rx, bodyTop + 2 * ry), 270f, -90f, false)//top left corner.
+        path.rLineTo(0f, heightMinusCorners)
+
+        path.arcTo(RectF(left, bottom - 2 * ry, left + 2 * rx, bottom), 180f, -90f, false) //bottom left corner
+        path.rLineTo(widthMinusCorners, 0f)
+
+        path.arcTo(RectF(right - 2 * rx, bottom - 2 * ry, right, bottom), 90f, -90f, false) //bottom right corner
         path.rLineTo(0f, -heightMinusCorners)
 
-        path.addCircle(bottom - (top - 100), (right - left / 2), 50f, Path.Direction.CCW) //todo testing
 
-        path.close()//Given close, last lineto can be removed.
+        //draw tip
+        val tipLeft = left + (width / 4)
+        val tipRight = left + (width / 2 + width / 4)
+        val tipTop = top
+        val tipBottom = top + 2 * ry
+
+        val tipPath = roundedRectArcApi14(tipLeft, tipTop, tipRight, tipBottom, rx, ry, conformToOriginalPost)
+        path.addPath(tipPath)
+
+        path.close()
+
+        return path
+    }
+
+
+    fun getBodyPath(left: Float, top: Float, right: Float, bottom: Float, rx: Float, ry: Float, paddingTop: Float): Path {
+        val bodyTop = top + paddingTop - paddingTop / 3
+        return roundedRectArcApi14(left + rx, bodyTop + ry, right - rx, bottom - ry, rx, ry, false)
+    }
+
+//    fun getSectionPath(left: Float, top: Float, right: Float, bottom: Float, rx: Float, ry: Float, paddingTop: Float): Path {
+//        val sectionCount = 6f
+//        val bodyTop = top + paddingTop - paddingTop / 3
+//
+//
+//        return roundedRectArcApi14(left + rx, bodyTop + ry, right - rx,bottom-(bottom/sectionCount) - ry, rx, ry, false)
+//    }
+
+    @Deprecated("test")
+    fun test(left: Float, top: Float, right: Float, bottom: Float, cr: Float, conformToOriginalPost: Boolean): Path {
+        val path = Path()
+        val width = right - left
+        val height = bottom - top
+
+        var leftP: Float
+        var topP: Float
+        var rightP: Float
+        var bottomP: Float
+
+
+        //Set cursor
+        path.moveTo(width - cr, cr)
+
+        //1-2
+        leftP = width - 2 * cr
+        topP = top + cr
+        rightP = right
+        bottomP = top + 2 * cr
+
+        path.arcTo(RectF(leftP, topP, rightP, bottomP), 0f, -90f, false) //top right corner
+
+        //2-3
+        path.lineTo(right, height - cr)
+
+        //3-4
+        leftP = right - cr
+        topP = bottom - cr
+        rightP = right
+        bottomP = bottom
+        path.arcTo(RectF(leftP, topP, rightP, bottomP), 90f, -90f, false) //bottom right corner
+
+        //4-5
+        path.lineTo((right - left) + cr, bottom)
+
+        //5-6
+        leftP = left
+        topP = bottom - cr
+        rightP = (right - left) + cr
+        bottomP = bottom
+        path.arcTo(RectF(leftP, topP, rightP, bottomP), 180f, -90f, false)  //bottom left corner
+
+        //6-7
+        path.lineTo(left, (cr * 2))
+
+        //7-8
+        leftP = left
+        topP = cr
+        rightP = cr
+        bottomP = cr
+        path.arcTo(RectF(leftP, topP, rightP, bottomP), 270f, -90f, false)  //top left corner
+
+        //8-9
+        path.lineTo((width / 4) - cr * 2, cr)
+
+        //9-10
+        leftP = (width / 4) - cr
+        topP = top
+        rightP = cr
+        bottomP = top + cr
+        path.arcTo(RectF(leftP, topP, rightP, bottomP), 270f, -90f, false) //tip left corner
+
+        //10-11
+        path.lineTo((width / 2 - width / 4) - cr, top)
+
+        //11-12
+        leftP = (width / 2 - width / 4) - cr
+        topP = cr
+        rightP = (width / 2 - width / 4)
+        bottomP = cr
+        path.arcTo(RectF(leftP, topP, rightP, bottomP), 0f, -90f, false) //tip right
+
+        //12-13
+        path.lineTo(width - cr, cr)
+
+        path.close()
+
         return path
     }
 
